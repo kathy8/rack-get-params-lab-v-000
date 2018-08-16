4f4a4d8b52b81bdbf7bf84e91fbb0e148308b6b6
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-08-16 20:07:50 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,26 +5,48 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
- elsif req.path.match(/search/)
-   search_term = req.params["q"]
-    resp.write handle_search(search_term)
-  elsif req.path.match(/cart/)
-     if @@cart.empty?
-       resp.write "Your cart is empty"
-     else
-       @@cart.each do |item|
-         resp.write "#{item}\n"
-       end
-     end
-   elsif req.path.match(/add/)
-     add_item = req.params["item"]
-     if @@items.include?(add_item)
-       @@cart << add_item
-       resp.write "added #{add_item}"
-     else
-       resp.write "We don't have that item"
-     end
-     else
-     resp.write "Path Not Found"
-   end
+    if req.path.match(/items/)
+             @@items.each do |item|
+               resp.write "#{item}\n"
+             end
+           elsif req.path.match(/search/)
+             search_term = req.params["q"]
+             resp.write handle_search(search_term)
+           elsif req.path.match(/cart/)
+             resp.write handle_cart
+           elsif req.path.match(/add/)
+             item = req.params["item"]
+             resp.write handle_add(item)
+           else
+            resp.write "Path Not Found"
+           end
+           resp.finish
+        end
+         def handle_search(search_term)
+          if @@items.include?(search_term)
+            return "#{search_term} is one of our items"
+          else
+            return "Couldn't find #{search_term}"
+          end	     
+        end
+
+      def handle_cart
+         if @@cart.size == 0
+        "Your cart is empty"
+      else
+        @@cart.join("\n")
+      end 
+    end 
+    def handle_add(item)
+ if @@items.include?(item)
+   @@cart << item
+   "added #{item}"
+ else
+     "We don't have that item"
+ end
+ # binding.pry
+ end
+ end 
+ 
