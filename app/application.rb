28f65cb534a3f5d8335ccc59e1c09f45bb8a4f9c
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-08-16 19:56:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,30 +7,29 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
+ elsif req.path.match(/search/)
+   search_term = req.params["q"]
+    resp.write handle_search(search_term)
+    
+  elsif req.path.match(/cart/)
+     if @@cart.empty?
+       resp.write "Your cart is empty"
+     else 
+       @@cart.each do |item|
+         resp.write "#{item}\n"
+       end
+     end
+   elsif req.path.match(/add/)
+     add_term = req.params["item"]
+     if @@items.include?(add_term)
+       @@cart << add_term
+       resp.write "added #{add_term}"
+     else
+       resp.write "We don't have that item"
+     end
 
-      @@items.each do |item|
-        resp.write "#{item}\n"
-      end
+   else
+     resp.write "Path Not Found"
+   end 
 
-      eslif req.path.match(/cart/)
-      @@cart.each do |item|
-      resp.write "#{item}\n"
-    end
-
-    if @@cart.empty?
-      resp.write "Your cart is empty"
-    end
-
-
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    elsif req.path.match(/add/)
-      add_item = req.params["item"]
-      @@cart << add_item
-      resp.write "added #{add_item}\n"
-    else
-      resp.write "We don't have that item"
-    end
-      resp.write "Path Not Found"
-    end
+    
