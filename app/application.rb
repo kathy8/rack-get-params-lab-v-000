2262d09897574ac6d28aace207e8aa06b8a815c8
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-08-16 19:44:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,12 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+
 
     if req.path.match(/items/)
       @@items.each do |item|
